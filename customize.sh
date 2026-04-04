d3be7c2decea8b835a364fb711dd4bece8a0e0ff
Drizzy_07 <42722790+Drizzy07x@users.noreply.github.com>
2026-04-04 07:45:14 -0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@ MODEL=$(getprop ro.product.model)
 
 ui_print "*********************************************************"
 ui_print "  PIXEL 9 PRO SERIES SUPERCHARGER 🚀"
-ui_print "  Build: v2.1 BETA.3"
+ui_print "  Build: v2.1 STABLE"
 ui_print "*********************************************************"
 
 if [ "$DEVICE" != "komodo" ] && [ "$DEVICE" != "caiman" ] && [ "$DEVICE" != "comet" ]; then
@@ -22,7 +22,7 @@ ui_print " |  _ \(_)_  _____| |  / _ \ "
 ui_print " | |_) | \ \/ / _ \ | | (_) |"
 ui_print " |  __/| |>  <  __/ |  \__, |"
 ui_print " |_|   |_/_/\_\___|_|    /_/ "
-ui_print "  S U P E R C H A R G E R  v2.1 BETA.3"
+ui_print "  S U P E R C H A R G E R  v2.1 STABLE"
 ui_print " "
 
 ui_print "========================================================="
